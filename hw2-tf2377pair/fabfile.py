7b5c53ca019f758d287e8fe663a029cd942088ae
Tianhua Fang <tianhuaf0526@gmail.com>
2016-10-12 21:51:42 -0400

    add fabric build script

---
+++ hw2-tf2377pair/fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import local
+
+def test():
+	local("python ./test_groupwise.py")
+#	local("./groupwise.py")
+
+
+def run():
+	local("python ./groupwise.py")
+
+
+def commit():
+	local("git add -p && git commit")
+
+def push()
+	local("git push")
+
+""" After we decoded the server to hold our code, we will add it
+def prepare_deploy():
+	test()
+	commit()
+	push()
+
+"""
