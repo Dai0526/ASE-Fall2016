764a52397e11b5bb9ee169d8e8c0bd7b4c20fb35
mona <zhangruomeng2@gmail.com>
2016-11-14 11:43:50 -0500

    add test cases for cache user problem

--- project/test_flaskr.py
+++ project/test_flaskr.py
@@ -99,5 +99,24 @@ class FlaskrTestCase(unittest.TestCase):
         ), follow_redirects=True)
         assert 'Invalid username' in rv.data
 
+    def test_cache(self):
+        self.register('myname', 'my@email', 'mypassword', 'mypassword')
+        self.login('myname','mypassword')
+        self.logout()
+        # In add message
+        rv = self.app.post('/add_message', data=dict(
+        text='Hello World!'
+        ), follow_redirects=True)
+        assert 'please login first' in rv.data
+        # In create group
+        rv = self.app.post('/creategroup', data=dict(
+        groupname='groupname1',
+        description='This is a description'
+        ), follow_redirects=True)
+        assert 'please login first' in rv.data
+
+        rv = self.app.get( '/groups' , follow_redirects=True)
+        assert 'please login first' in rv.data
+
 if __name__ == '__main__':
     unittest.main()
